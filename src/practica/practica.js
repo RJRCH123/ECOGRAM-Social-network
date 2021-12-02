ff303c2c0dce83d5023bfb013b8557adc02723ec
Rosamaria Rodriguez <rous9716@gmail.com>
2021-12-02 12:03:19 -0500

    Page Home general responsive

--- src/practica/practica.js
+++ src/practica/practica.js
@@ -3,16 +3,16 @@
 const navToggle = document.querySelector('.NavToggle');
 const navMenu = document.querySelector('.nav-menu');
 
-/********** BAR NAVEGATION ************/
+// BAR NAVEGATION
 
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('nav-menu_visible');
 
-// Event for area-label open and close menu
+  // Event for area-label open and close menu
 
-if (navMenu.classList.contains('nav-menu_visible')){
-  navToggle.setAttribute('aria-label', 'Close menu');
-}else {
-  navToggle.setAttribute('aria-label', 'Open menu')
-}
+  if (navMenu.classList.contains('nav-menu_visible')) {
+    navToggle.setAttribute('aria-label', 'Close menu');
+  } else {
+    navToggle.setAttribute('aria-label', 'Open menu')
+  }
 });

27c77d39f4faa254c395c694555fee65c76075a4
Rosamaria Rodriguez <rous9716@gmail.com>
2021-12-17 10:17:49 -0500

    Delete files and carpets

--- src/pages/preBienvenida.js
+++
@@ -1,7 +0,0 @@
-export const viewPreBienv = () => {
-  window.addEventListener('load', () => {
-    const contenedorLoader = document.querySelector('.contenedorLoader');
-    contenedorLoader.style.opacity = 0;
-    contenedorLoader.style.visibility = 'hidden';
-  });
-};
